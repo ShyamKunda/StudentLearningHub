85bb834d7fe29576399b361d71d5b5915741e367
shyam.k <shyam.k@turvo.com>
2020-04-18 12:21:34 +0530

    Changing options index 1 to 0

--- src/main/java/inquerro/model/MiniQuestion.java
+++ src/main/java/inquerro/model/MiniQuestion.java
@@ -10,10 +10,10 @@ import java.util.List;
 public class MiniQuestion {
 
     private String content;
+    private String option0;
     private String option1;
     private String option2;
     private String option3;
-    private String option4;
     private String answer;
     private List<String> tags2;
     private String explanation;
