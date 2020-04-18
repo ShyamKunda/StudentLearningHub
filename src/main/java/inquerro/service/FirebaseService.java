e5e9a67a2f0593889b8b4b8e1add47583d943e13
shyam.k <shyam.k@turvo.com>
2020-04-18 12:23:22 +0530

    Changing firebase initialisation from method to instance level

--- src/main/java/inquerro/service/FirebaseService.java
+++ src/main/java/inquerro/service/FirebaseService.java
@@ -19,28 +19,26 @@ import java.util.concurrent.ExecutionException;
 @Service
 public class FirebaseService {
 
-    public FirebaseService() {
+    Firestore firestore;
 
+    public FirebaseService() {
+        firestore = FirestoreClient.getFirestore();
     }
 
     public String  saveQuestion(Question question) throws ExecutionException, InterruptedException {
 
-
-        Firestore firestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionsApiFuture =  firestore.collection("Questions").document().set(question);
         return collectionsApiFuture.get().getUpdateTime().toString();
     }
 
     public String  saveUser(UserRegistrationDto user) throws ExecutionException, InterruptedException {
 
-
-        Firestore firestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionsApiFuture =  firestore.collection("User").document().set(user);
         return collectionsApiFuture.get().getUpdateTime().toString();
     }
 
     public Long getQuestionsCount() throws ExecutionException, InterruptedException {
-        Firestore firestore = FirestoreClient.getFirestore();
+
         Long totalQuestions = firestore.collection("Stats").document("questions").get().get().getLong("count");
         QuerySnapshot querySnapshot =  firestore.collection("Questions").orderBy("id", Query.Direction.DESCENDING).limit(1).get().get();
         return querySnapshot.getDocuments().get(0).getLong("id");
@@ -48,7 +46,6 @@ public class FirebaseService {
 
     public String setQuestionsCount(Long count) throws ExecutionException, InterruptedException {
 
-        Firestore firestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionsApiFuture = firestore.collection("Stats").document("questions/id").set(count);
 
 
@@ -57,14 +54,12 @@ public class FirebaseService {
 
     public String saveMathJaxEquation(MathJaxVerification mathJaxVerification) throws ExecutionException, InterruptedException {
 
-        Firestore firestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionsApiFuture = firestore.collection("MathJaxValidation").document("1").set(mathJaxVerification);
         return collectionsApiFuture.get().getUpdateTime().toString();
     }
 
     public String getEquation() throws ExecutionException, InterruptedException {
 
-        Firestore firestore = FirestoreClient.getFirestore();
         String  data = firestore.collection("MathJaxValidation").document("1").get().get().get("data").toString();
         return data;
     }
